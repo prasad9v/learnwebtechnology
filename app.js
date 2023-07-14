96826bfc6af7ed22707895236ca1bc67d57b343d
prasad9v <prasad9.v@gmail.com>
2023-07-14 19:38:30 +0530

    object properties added

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+let mouse = {
+  brand:'zebronics',
+  price:300,
+  refund:true,
+  buttons:6,
+  interface:'usb',
+  cableLength:1.8
+}
 
-const messageEl = document.querySelector('#messaage')
+const messageEl = document.querySelector('#message')
 const displayEl = document.querySelector('#display')
+const mouseEl = document.querySelector('#mouse');
 
 const reverseMessage = e => displayEl.innerText = [...messageEl.textContent].reverse().join('')
 const clearDisplay = e => displayEl.innerText = ''
+const displayObject = e => mouseEl.innerText= mouse.brand 
